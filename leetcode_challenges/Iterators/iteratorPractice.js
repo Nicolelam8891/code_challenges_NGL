c9d60a7dd9a8530365f37d9f0fb3f315340d76a8
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-04-21 20:34:40 -0600

    Add practice problem for iterator practice

---
+++ leetcode_challenges/Iterators/iteratorPractice.js
@@ -0,0 +1,9 @@
+// Level 1: Beginner
+
+const sentences = [
+  "This is the first sentence.",
+  "A second sentence follows.",
+  "The third sentence is here."
+];
+
+// Prompt: Write a function that takes an array of sentences and returns a new array with each sentence doubled.
